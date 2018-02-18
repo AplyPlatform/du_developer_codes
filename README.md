b20ff9c7bd8a24978f75383ed9d800389eb0de05
Gunwoo Lee <gunman@posicube.com>
2018-02-18 23:38:41 +0900

    안드로이드 예제 코드 추가

--- README.md
+++ README.md
@@ -1,5 +1,33 @@
 # 드론플레이 Codes
 
 ## 경로설명
-> [/api](https://github.com/theknightsfield/droneplaycodes/tree/master/api/) : OpenAPI 를 활용하는 예제 코드들이 있습니다.
 
+> [/api/web](https://github.com/theknightsfield/droneplaycodes/tree/master/api/web) : Open API를 활용하는 웹 기반의 예제 코드.
+> [/api/android](https://github.com/theknightsfield/droneplaycodes/tree/master/api/android) : Open API를 활용하는 안드로이드 기반의 예제 코드.
+
+# DronePlayMission Code - 안드로이드 
+![Alt text](/api/screen.jpg "DronePlayMission")
+
+* 지도에 터치하여 마크가 된 장소로 드론이 모두 방문하는 코드 예제 입니다.
+ * 각 장소를 방문할때 마다 해당 장소의 좌표를 DronePlay API를 호출하여 기록합니다.
+ *코드의 수행을 위해 Manifest 파일에 구글 지도 API KEY, DJI SDK API KEY, DRONEPLAY API TOKEN 정보를 입력해야 합니다.
+
+> AndroidManifest.xml
+<pre>
+<code>
+	<meta-data
+            android:name="com.google.android.geo.API_KEY"
+            android:value="GOOGLE-MAP-API-KEY" />
+        <meta-data
+            android:name="com.dji.sdk.API_KEY"
+            android:value="DJI-SDK-API-KEY"/>
+
+        <meta-data
+            android:name="io.droneplay.token"
+            android:value="DRONEPLAY-API-TOKEN"/>
+
+        <meta-data
+            android:name="io.droneplay.email"
+            android:value="DRONEPLAY-API-EMAIL"/>
+</code>
+</pre>
---
+++ api/android/DronePlayMission/app/src/main/java/io/droneplay/droneplaymission/WaypointManager.java
@@ -0,0 +1,161 @@
+package io.droneplay.droneplaymission;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import dji.common.mission.waypoint.Waypoint;
+import dji.common.mission.waypoint.WaypointAction;
+import dji.common.mission.waypoint.WaypointActionType;
+import dji.common.mission.waypoint.WaypointMission;
+import dji.common.mission.waypoint.WaypointMissionFinishedAction;
+import dji.common.mission.waypoint.WaypointMissionFlightPathMode;
+import dji.common.mission.waypoint.WaypointMissionGotoWaypointMode;
+import dji.common.mission.waypoint.WaypointMissionHeadingMode;
+import dji.keysdk.FlightControllerKey;
+import dji.keysdk.KeyManager;
+
+import static dji.keysdk.FlightControllerKey.HOME_LOCATION_LATITUDE;
+import static dji.keysdk.FlightControllerKey.HOME_LOCATION_LONGITUDE;
+
+/**
+ * Created by gunman on 2018. 2. 12..
+ */
+
+public class WaypointManager {
+
+    private static final double ONE_METER_OFFSET = 0.00000899322;
+    private List<Waypoint> mwaypointList;
+
+    public WaypointManager() {
+        mwaypointList = new ArrayList<>();
+    }
+
+
+    public void clear() {
+        mwaypointList.clear();
+    }
+
+    public int addWaypointMission(double baseLatitude, double baseLongitude, float baseAltitude, int action) {
+        Waypoint eachWaypoint = new Waypoint(baseLatitude,baseLongitude, baseAltitude);
+
+
+                switch (action) {
+                    case 0:
+                        eachWaypoint.addAction(new WaypointAction(WaypointActionType.STAY, 1));
+                        break;
+                    case 1:
+                        eachWaypoint.addAction(new WaypointAction(WaypointActionType.START_TAKE_PHOTO, 1));
+                        break;
+                    case 2:
+                        eachWaypoint.addAction(new WaypointAction(WaypointActionType.START_RECORD, 1));
+                        eachWaypoint.addAction(new WaypointAction(WaypointActionType.STOP_RECORD, 1));
+                        break;
+                    /*
+                    case 3:
+                        eachWaypoint.addAction(new WaypointAction(WaypointActionType.GIMBAL_PITCH,
+                                randomGenerator.nextInt() % 45 - 45));
+                        break;
+                    case 4:
+                        eachWaypoint.addAction(new WaypointAction(WaypointActionType.ROTATE_AIRCRAFT,
+                                randomGenerator.nextInt() % 180));
+                        break;
+                    */
+
+                    default:
+                        eachWaypoint.addAction(new WaypointAction(WaypointActionType.START_TAKE_PHOTO, 1));
+                        break;
+                }
+
+        mwaypointList.add(eachWaypoint);
+
+        return mwaypointList.size();
+    }
+
+    public WaypointMission getWaypointMission() {
+        if (mwaypointList.size() <= 0) return null;
+        WaypointMission.Builder mbuilder = new WaypointMission.Builder();
+
+        mbuilder.autoFlightSpeed(5f);
+        mbuilder.maxFlightSpeed(10f);
+        mbuilder.setExitMissionOnRCSignalLostEnabled(false);
+        mbuilder.finishedAction(WaypointMissionFinishedAction.NO_ACTION);
+        mbuilder.flightPathMode(WaypointMissionFlightPathMode.NORMAL);
+        mbuilder.gotoFirstWaypointMode(WaypointMissionGotoWaypointMode.SAFELY);
+        mbuilder.headingMode(WaypointMissionHeadingMode.AUTO);
+        mbuilder.repeatTimes(1);
+
+        mbuilder.waypointList(mwaypointList).waypointCount(mwaypointList.size());
+        return mbuilder.build();
+    }
+
+    /**
+     * Randomize a WaypointMission
+     *
+     * @param numberOfWaypoint total number of Waypoint to randomize
+     * @param numberOfAction total number of Action to randomize
+     */
+    public WaypointMission createRandomWaypointMission(int numberOfWaypoint, int numberOfAction) {
+        WaypointMission.Builder builder = new WaypointMission.Builder();
+        double baseLatitude = 22;
+        double baseLongitude = 113;
+        Object latitudeValue = KeyManager.getInstance().getValue((FlightControllerKey.create(HOME_LOCATION_LATITUDE)));
+        Object longitudeValue =
+                KeyManager.getInstance().getValue((FlightControllerKey.create(HOME_LOCATION_LONGITUDE)));
+        if (latitudeValue != null && latitudeValue instanceof Double) {
+            baseLatitude = (double) latitudeValue;
+        }
+        if (longitudeValue != null && longitudeValue instanceof Double) {
+            baseLongitude = (double) longitudeValue;
+        }
+
+        final float baseAltitude = 20.0f;
+        builder.autoFlightSpeed(5f);
+        builder.maxFlightSpeed(10f);
+        builder.setExitMissionOnRCSignalLostEnabled(false);
+        builder.finishedAction(WaypointMissionFinishedAction.NO_ACTION);
+        builder.flightPathMode(WaypointMissionFlightPathMode.NORMAL);
+        builder.gotoFirstWaypointMode(WaypointMissionGotoWaypointMode.SAFELY);
+        builder.headingMode(WaypointMissionHeadingMode.AUTO);
+        builder.repeatTimes(1);
+        Random randomGenerator = new Random(System.currentTimeMillis());
+        List<Waypoint> waypointList = new ArrayList<>();
+        for (int i = 0; i < numberOfWaypoint; i++) {
+            final double variation = (Math.floor(i / 4) + 1) * 2 * ONE_METER_OFFSET;
+            final float variationFloat = (baseAltitude + (i + 1) * 2);
+            final Waypoint eachWaypoint = new Waypoint(baseLatitude + variation * Math.pow(-1, i) * Math.pow(0, i % 2),
+                    baseLongitude + variation * Math.pow(-1, (i + 1)) * Math.pow(0, (i + 1) % 2),
+                    variationFloat);
+            for (int j = 0; j < numberOfAction; j++) {
+                final int randomNumber = randomGenerator.nextInt() % 6;
+                switch (randomNumber) {
+                    case 0:
+                        eachWaypoint.addAction(new WaypointAction(WaypointActionType.STAY, 1));
+                        break;
+                    case 1:
+                        eachWaypoint.addAction(new WaypointAction(WaypointActionType.START_TAKE_PHOTO, 1));
+                        break;
+                    case 2:
+                        eachWaypoint.addAction(new WaypointAction(WaypointActionType.START_RECORD, 1));
+                        eachWaypoint.addAction(new WaypointAction(WaypointActionType.STOP_RECORD, 1));
+                        break;
+                    case 3:
+                        eachWaypoint.addAction(new WaypointAction(WaypointActionType.GIMBAL_PITCH,
+                                randomGenerator.nextInt() % 45 - 45));
+                        break;
+                    case 4:
+                        eachWaypoint.addAction(new WaypointAction(WaypointActionType.ROTATE_AIRCRAFT,
+                                randomGenerator.nextInt() % 180));
+                        break;
+                    default:
+                        eachWaypoint.addAction(new WaypointAction(WaypointActionType.START_TAKE_PHOTO, 1));
+                        break;
+                }
+            }
+            waypointList.add(eachWaypoint);
+        }
+        builder.waypointList(waypointList).waypointCount(waypointList.size());
+        return builder.build();
+    }
+
+}
